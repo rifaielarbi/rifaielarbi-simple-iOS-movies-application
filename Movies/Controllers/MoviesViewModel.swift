1eb2ce1c1785803d55cac7a4f47952a4c76b0da7
rifaielarbi <rifayarabi@gmail.com>
2024-08-27 17:39:39 +0100

    first commit

---
+++ Movies/Controllers/MoviesViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesViewModel.swift
+//  Movies
+//
+//  Created by Arabi's Mac on 27/8/2024.
+//
+
+import UIKit
+
+class MoviesViewModel: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Movies/Services/NetworkLayer.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkLayer.swift
+//  Movies
+//
+//  Created by Arabi's Mac on 26/8/2024.
+//
+
+import Foundation
